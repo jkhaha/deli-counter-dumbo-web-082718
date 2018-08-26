fa63e56e35d58d883c79a367810bb8dbedecad37
Jackie Ha <jha2@wellesley.edu>
2018-08-26 17:06:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,18 @@
-# line method "The line is currently empty."
-katz_deli = []
-# beginning of day, deli is empty
-
-def line(katz_deli)
-  if katz_deli.length === 0
-  returns "The line is currently empty."
-end
-
-# take_a_number method, accepts two arguments
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name},you are number #{katz_deli.length} in line."
-end
+def take_a_number(katz_deli name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end 
 
 def now_serving(katz_deli)
-  if (katz_deli.length === 0)
-  puts "There is nobody waiting to be served!"
-else
-  puts "Currently serving #{katz_deli.shift}"
-end
+  if katz_deli.length = 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli.shift}."
+  end 
+end 
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    put
